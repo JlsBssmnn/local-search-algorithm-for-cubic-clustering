641043b46097f4039dabfb2fe4790477a9cf23b2
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2022-05-21 11:55:19 +0200

    add linked list implementation

---
+++ src/utils/LinkedList.go
@@ -0,0 +1,70 @@
+package utils
+
+type Node[T any] struct {
+	content T
+	next    *Node[T]
+}
+
+type LinkedList[T any] struct {
+	firstElement *Node[T]
+	lastElement  *Node[T]
+	length       int
+}
+
+func (list *LinkedList[T]) Add(element T) {
+	newElement := Node[T]{
+		content: element,
+	}
+
+	if list.length == 0 {
+		list.firstElement = &newElement
+	} else {
+		list.lastElement.next = &newElement
+	}
+	list.lastElement = &newElement
+	list.length++
+}
+
+func (list *LinkedList[T]) Length() int {
+	return list.length
+}
+
+func (list *LinkedList[T]) Get(i int) T {
+	if i == -1 || i == list.length-1 {
+		return list.lastElement.content
+	} else if i < -1 || i >= list.length {
+		panic("Index out of bounds")
+	}
+	index := 0
+	element := list.firstElement
+	for i != index {
+		element = element.next
+		index++
+	}
+	return element.content
+}
+
+func (list *LinkedList[T]) Iterator() Iterator[T] {
+	return Iterator[T]{
+		index:          0,
+		currentElement: list.firstElement,
+	}
+}
+
+type Iterator[T any] struct {
+	index          int
+	currentElement *Node[T]
+}
+
+func (iter *Iterator[T]) HasNext() bool {
+	return iter.currentElement != nil
+}
+
+func (iter *Iterator[T]) Next() T {
+	if !iter.HasNext() {
+		panic("Iterator has no next element")
+	}
+	element := iter.currentElement
+	iter.currentElement = iter.currentElement.next
+	return element.content
+}
---
+++ src/utils/LinkedList_test.go
@@ -0,0 +1,62 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdd(t *testing.T) {
+	list := LinkedList[int]{}
+	assert.Equal(t, 0, list.Length())
+
+	assert.Nil(t, list.firstElement)
+	assert.Nil(t, list.lastElement)
+
+	list.Add(1)
+
+	assert.Equal(t, 1, list.Length())
+	assert.NotNil(t, list.firstElement)
+	assert.NotNil(t, list.lastElement)
+	assert.Equal(t, 1, list.firstElement.content)
+
+	list.Add(6)
+
+	assert.Equal(t, 2, list.Length())
+	assert.NotNil(t, list.firstElement)
+	assert.NotNil(t, list.lastElement)
+	assert.Equal(t, 1, list.firstElement.content)
+	assert.Equal(t, 6, list.lastElement.content)
+
+	list.Add(4)
+
+	assert.Equal(t, 3, list.Length())
+	assert.NotNil(t, list.firstElement)
+	assert.NotNil(t, list.lastElement)
+	assert.Equal(t, 1, list.firstElement.content)
+	assert.Equal(t, 6, list.firstElement.next.content)
+	assert.Equal(t, 4, list.lastElement.content)
+}
+
+func TestIterator(t *testing.T) {
+	list := LinkedList[int]{}
+	list.Add(5)
+	list.Add(3)
+	list.Add(21)
+	list.Add(32)
+	list.Add(-2)
+
+	iter := list.Iterator()
+	assert.True(t, iter.HasNext())
+
+	assert.Equal(t, 5, iter.Next())
+	assert.Equal(t, 3, iter.Next())
+	assert.Equal(t, 21, iter.Next())
+	assert.Equal(t, 32, iter.Next())
+	assert.Equal(t, -2, iter.Next())
+
+	assert.False(t, iter.HasNext())
+	assert.Panics(t, func() {
+		iter.Next()
+	})
+}
