f8e188e9694d2b434a6d5b59b3be81e31a37d35e
JlsBssmnn <julius.bossmann@mailbox.tu-dresden.de>
2022-07-06 15:54:20 +0200

    use config files for eval parameters

--- src/cmd/runFixedEvaluation/main.go
+++ src/cmd/runFixedEvaluation/main.go
@@ -17,17 +17,20 @@ import (
 	"github.com/JlsBssmnn/local-search-algorithm-for-cubic-clustering/src/partitioning3D"
 	"github.com/JlsBssmnn/local-search-algorithm-for-cubic-clustering/src/partitioning3D/evaluation"
 	"github.com/JlsBssmnn/local-search-algorithm-for-cubic-clustering/src/utils"
+	"github.com/go-playground/validator/v10"
 	"github.com/vbauerster/mpb"
 	"github.com/vbauerster/mpb/decor"
 )
 
-const ITERATIONS = 45
-
-var STDDEV_VALUES = []float64{0.005, 0.01, 0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045, 0.05, 0.055, 0.06, 0.065, 0.07, 0.075, 0.08, 0.085, 0.09, 0.095, 0.1}
-
-const POINTS_PER_PLANE = 33
+var config EvalConfig
 
 var bar = mpb.New()
+
+type EvalConfig struct {
+	Iterations     int       `validate:"required,gt=0"`
+	StddevValues   []float64 `validate:"required,dive,gt=0"`
+	PointsPerPlane int       `validate:"required,gt=0"`
+}
 
 type Result struct {
 	GitCommit       string
@@ -59,13 +62,13 @@ func (result *Result) write() {
 func (result *Result) checkParameters(algorithm, commit string) {
 	var wrongParameter string
 	switch {
-	case result.Iterations != ITERATIONS:
+	case result.Iterations != config.Iterations:
 		wrongParameter = "Iterations"
 	case result.Algorithm != algorithm:
 		wrongParameter = "Algorithm"
-	case !utils.EqualSlices(result.StddevValues, STDDEV_VALUES):
+	case !utils.EqualSlices(result.StddevValues, config.StddevValues):
 		wrongParameter = "StddevValues"
-	case result.PointsPerPlane != POINTS_PER_PLANE:
+	case result.PointsPerPlane != config.PointsPerPlane:
 		wrongParameter = "PointsPerPlane"
 	}
 
@@ -87,10 +90,13 @@ func main() {
 	output := flag.String("output", "", "Where the output of the evaluation should be written to, the output will be in json")
 	verbose := flag.Int("verbose", 0, "0: nothing will be printed, 1: Progress bars will indicate the progress of the evaluation")
 	choice := flag.String("choice", "", "What to do if the file already exists, if specified the user will not be requested to give input")
+	configFile := flag.String("config", "./temp/eval_configs/default_config.json", "Which configuration file in src/temp/eval_configs will be used for the evaluation parameters")
 	flag.Parse()
 
 	seed := time.Now().Unix()
 	rand.Seed(seed)
+
+	loadParameters(*configFile)
 
 	if _, err := os.Stat(*output); *choice == "" && !errors.Is(err, os.ErrNotExist) {
 		fmt.Printf("The output file seems to exist already, do you want to overwrite it or continue your work or abort [o/c/a]?: ")
@@ -131,13 +137,14 @@ func main() {
 		result = Result{
 			GitCommit:       gitCommit,
 			Algorithm:       *selectedAlgorithm,
-			Iterations:      ITERATIONS,
-			StddevValues:    STDDEV_VALUES,
-			PointsPerPlane:  POINTS_PER_PLANE,
+			Iterations:      config.Iterations,
+			StddevValues:    config.StddevValues,
+			PointsPerPlane:  config.PointsPerPlane,
 			Seed:            seed,
-			AccuracyResults: make([]AccuracyResult, 0, len(STDDEV_VALUES)),
+			AccuracyResults: make([]AccuracyResult, 0, len(config.StddevValues)),
 			outputFile:      file,
 		}
+		result.write()
 	}
 
 	algorithm := algorithm.AlgorithmStringToFunc[geometry.Vector](*selectedAlgorithm)
@@ -146,7 +153,7 @@ func main() {
 
 	// Determine the starting point if execution of evalution is continued
 	stddevOffset := len(result.AccuracyResults)
-	if stddevOffset > 0 && len(result.AccuracyResults[stddevOffset-1].Accuracies) < ITERATIONS {
+	if stddevOffset > 0 && len(result.AccuracyResults[stddevOffset-1].Accuracies) < config.Iterations {
 		stddevOffset--
 	}
 
@@ -154,28 +161,28 @@ func main() {
 	if len(result.AccuracyResults)-1 == stddevOffset {
 		iterationOffset = len(result.AccuracyResults[stddevOffset].Accuracies)
 	} else {
-		result.AccuracyResults = append(result.AccuracyResults, AccuracyResult{Accuracies: make([]float64, 0, ITERATIONS), Time: 0})
+		result.AccuracyResults = append(result.AccuracyResults, AccuracyResult{Accuracies: make([]float64, 0, config.Iterations), Time: 0})
 	}
 
 	fixSeed(stddevOffset, iterationOffset, planes)
 
-	mainPb := createPb(int64(len(STDDEV_VALUES)), "Stddev values:", *verbose, stddevOffset)
+	mainPb := createPb(int64(len(config.StddevValues)), "Stddev values:", *verbose, stddevOffset)
 
-	for i := stddevOffset; i < len(STDDEV_VALUES); i++ {
+	for i := stddevOffset; i < len(config.StddevValues); i++ {
 		stddev := result.StddevValues[i]
 
 		var startIteration int
 		if i == stddevOffset {
 			startIteration = iterationOffset
 		} else {
-			result.AccuracyResults = append(result.AccuracyResults, AccuracyResult{Accuracies: make([]float64, 0, ITERATIONS), Time: 0})
+			result.AccuracyResults = append(result.AccuracyResults, AccuracyResult{Accuracies: make([]float64, 0, config.Iterations), Time: 0})
 		}
 
-		var secondaryPb = createPb(int64(ITERATIONS), "iterations:", *verbose, startIteration)
+		var secondaryPb = createPb(int64(config.Iterations), "iterations:", *verbose, startIteration)
 
-		for j := startIteration; j < ITERATIONS; j++ {
+		for j := startIteration; j < config.Iterations; j++ {
 			start := time.Now()
-			testData := evaluation.GenerateDataFromPlanesWithNoise(planes, POINTS_PER_PLANE, utils.NormalDist{Mean: 0, Stddev: stddev})
+			testData := evaluation.GenerateDataFromPlanesWithNoise(planes, config.PointsPerPlane, utils.NormalDist{Mean: 0, Stddev: stddev})
 			calc := partitioning3D.CostCalculator{Threshold: 3 * stddev, Amplification: 1 / stddev}
 			eval := evaluation.EvaluateAlgorithm(algorithm, &calc, &testData)
 
@@ -221,7 +228,28 @@ func createPb(total int64, name string, verbose, startProgress int) *mpb.Bar {
 // This function generates random data so often that the seed which was
 // used in a prior execution now matches with this execution
 func fixSeed(startStddevValue, iterationStart int, planes []geometry.Vector) {
-	for i := 0; i < startStddevValue*ITERATIONS+iterationStart; i++ {
-		evaluation.GenerateDataFromPlanesWithNoise(planes, POINTS_PER_PLANE, utils.NormalDist{Mean: 0, Stddev: 0})
+	for i := 0; i < startStddevValue*config.Iterations+iterationStart; i++ {
+		evaluation.GenerateDataFromPlanesWithNoise(planes, config.PointsPerPlane, utils.NormalDist{Mean: 0, Stddev: 0})
+	}
+}
+
+// This function loads the config in the given file
+func loadParameters(filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	fileContent, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	json.Unmarshal(fileContent, &config)
+
+	validator := validator.New()
+	err = validator.Struct(config)
+	if err != nil {
+		panic("Config file is invalid")
 	}
 }
